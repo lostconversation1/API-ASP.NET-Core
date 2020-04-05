8452c03abb4fc0ef58f3bb179318972b15ec053f
gosia <malgorzataflak4@gmail.com>
2020-04-05 21:41:21 +0200

    returning child resources GET api/cities/:cityId/pointsofinterest GET api/cities/:cityId/pointsofinterest/:id

--- CityInfo/CityInfo.API/CitiesDataStore.cs
+++ CityInfo/CityInfo.API/CitiesDataStore.cs
@@ -19,19 +19,64 @@ namespace CityInfo.API
                 {
                     Id=1,
                     Name="DG City",
-                    Description="thE one with park"
+                    Description="thE one with park",
+                    PointsOfInterest= new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id=1,
+                            Name="1ssss as",
+                            Description= "1test xvxvxvxv"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id=2,
+                            Name="2ssss as",
+                            Description= "2test xvxvxvxv"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id=2,
                     Name="Bedzin",
-                    Description="regular one"
+                    Description="regular one",
+                     PointsOfInterest= new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id=3,
+                            Name="14ssss as",
+                            Description= "14test xvxvxvxv"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id=4,
+                            Name="23ssss as",
+                            Description= "23test xvxvxvxv"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id=3,
                     Name="Sosnowiec",
-                    Description="the one from jokes"
+                    Description="the one from jokes",
+                     PointsOfInterest= new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id=5,
+                            Name="51ssss as",
+                            Description= "51test xvxvxvxv"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id=6,
+                            Name="26ssss as",
+                            Description= "62test xvxvxvxv"
+                        }
+                    }
                 }
             };
         }
