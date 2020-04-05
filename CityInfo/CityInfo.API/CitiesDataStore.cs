dd868e7c3f76fdeaad072a9b85d250e7597c6e19
gosia <malgorzataflak4@gmail.com>
2020-04-05 18:29:50 +0200

    Create
    GET api/cities
    GET api/cities/:id

---
+++ CityInfo/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,39 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id=1,
+                    Name="DG City",
+                    Description="thE one with park"
+                },
+                new CityDto()
+                {
+                    Id=2,
+                    Name="Bedzin",
+                    Description="regular one"
+                },
+                new CityDto()
+                {
+                    Id=3,
+                    Name="Sosnowiec",
+                    Description="the one from jokes"
+                }
+            };
+        }
+    }
+}
